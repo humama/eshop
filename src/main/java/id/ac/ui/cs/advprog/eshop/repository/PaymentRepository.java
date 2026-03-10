3d59a424581f9dc6ab9eb793683fedf5c78599c0
Humam Al Labib <humam.alabib@gmail.com>
2026-03-10 19:24:45 +0700

    [GREEN] Implement PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -13,14 +13,30 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
-        return null;
+        int ii = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.remove(ii);
+                paymentData.add(ii, payment);
+                return payment;
+            }
+            ii += 1;
+        }
+
+        paymentData.add(payment);
+        return payment;
     }
 
     public Payment findById(String id) {
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
         return null;
     }
 
     public List<Payment> findAll() {
-        return null;
+        return paymentData;
     }
 }
